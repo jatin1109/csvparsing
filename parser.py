9ff2b5bc767d42e9e6036a1e653e0f34447bd26e
jatin1109 <45520289+jatin1109@users.noreply.github.com>
2018-12-01 14:57:56 -0800

    parser.py
    Changing the string values to floats.

--- parser.py
+++ parser.py
@@ -2,12 +2,22 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
+def RepresentsInt(s):
+    try: 
+        float(s)
+        return True
+    except ValueError:
+        return False
 file = csv.reader(open('student_data.csv'))
 list_of_lists_as_rows = [] 
 list_of_lists_as_columns = []
 no_of_columns = 0
 for row in file:
-    list_of_lists_as_rows.append(row)
+    row1 = row
+    for i in row1:
+        if RepresentsInt(i):
+            i = float(i)
+    list_of_lists_as_rows.append(row1)
     no_of_columns = len(row)
     
 for i in range(no_of_columns):
@@ -18,6 +28,8 @@ for i in range(no_of_columns):
         if x == 1:
             x += 1
             continue
+        if RepresentsInt(row[i]):
+            row[i] = float(row[i])
         column.append(row[i])
     list_of_lists_as_columns.append(column)
     
